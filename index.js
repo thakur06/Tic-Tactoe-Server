780c82f692780e2219000a5a0fee1e78474f8d26
ABHISHEK RAJPUT <79101220+thakur06@users.noreply.github.com>
2024-09-06 19:24:26 +0530

    changed server

--- index.js
+++ index.js
@@ -1,67 +1,24 @@
-import express from 'express';
-import cors from 'cors';
-import { StreamChat } from "stream-chat"
-import { v4 as uuidv4 } from "uuid"
-import dotenv from 'dotenv';
-import brcypt from "bcrypt"
+const express = require("express");
+const cors = require("cors");
 const app = express();
-app.use(cors(
-
-));
-app.use(express.urlencoded({extended:true}));
+require("dotenv").config();
+const PORT = process.env.PORT || 4000;
+app.use(cors());
 app.use(express.json());
-dotenv.config();
-const api_key =     process.env.API_key;
-const api_secret =     process.env.API_SECRET;
-
-const connection = StreamChat.getInstance(api_key, api_secret);
-app.post("/login", async (req, res) => {
-    const { name, password } = req.body;
-    try {
-        const { users } = await connection.queryUsers({ Uname: name });
-
-        if (users.length == 0) return res.json({ message: "User not found" })
-console.log(users[0])
-        const pass = await brcypt.compare(password, users[0].hashedPassword);
-        const token = connection.createToken(users[0].id);
-
-
-        if (pass) {
-
-            res.json({
-                token,
-                Fname: users[0].Fname,
-                Lname: users[0].Lname,
-                Uname: users[0].Uname,
-                uId: users[0].id
-            })
-        }
-        else{
-            res.status(401).send({mgs:"User not found",status:404});
-        }
-
-    } catch (error) {
-        res.json(error);
-    }
-
-})
-
-
-app.post("/signin", async (req, res) => {
-    try {
-        const { Fname, Lname, Uname, password } = req.body;
-        const uId = uuidv4();
-        const hashedPassword = await brcypt.hash(password, 10);
-        const token = connection.createToken(uId);
-        res.json({ token, Fname, Lname, Uname, uId, hashedPassword });
-    }
-    catch (err) {
-        res.json(err);
-    }
-})
+app.use(express.urlencoded({ extended: true }));
 app.get("/",(req,res)=>{
-    res.send("hi vercel cors not working");
+    res.send("hello world");
+});
+app.listen(PORT, (err) => {
+    if (!err) {
+        console.log(`app listening on post 4000`);
+
+    } else {
+        console.log(err);
+
+    }
 })
-app.listen(8000, () => {
-    console.log("running on 8000")
-})
+
+
+
+
